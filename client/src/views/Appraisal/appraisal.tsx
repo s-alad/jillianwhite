b959b2ea8ee5b64d9be846e093c8ab58de303106
s-alad <n7.saad@gmail.com>
2022-11-24 18:35:36 -0500

    -

--- client/src/views/Appraisal/appraisal.tsx
+++ client/src/views/Appraisal/appraisal.tsx
@@ -6,8 +6,12 @@ function Appraisal() {
     return (
         <div className="appraisal">
             <h1>Appraisal Work</h1>
-            <video>
-                <source src="https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/v1/video-secured-attachments/message/order_attachments/b562225bd08ec7dadd0941a99b238416-1669066728986/lianacoya?__cld_token__=exp=1669262659~hmac=2c95ab81898d1808a4077d2aa2cf67981230859a971dba9cf62a7262949a92d1" height="300" width="300"></source>
+            <p>
+            Motivated by her professional experience as an appraiser and personal experience whitewashing her family’s home in order to get a fair value, Jillian has made it her personal mission to educate and empower all homeowners by seeking fairness in appraisals.
+
+            </p>
+            <video controls>
+                <source src="https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/v1/video-secured-attachments/message/order_attachments/b562225bd08ec7dadd0941a99b238416-1669066728986/lianacoya?__cld_token__=exp=1669344920~hmac=8826342379a650377b2801cea7292713c8a8d0c0106018ecfe1032fa3b325b56"  height="300" width="300" type="video/mp4"></source>
             </video>
         </div>
     );
