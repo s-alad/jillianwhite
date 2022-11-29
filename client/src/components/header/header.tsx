c695423edcfb887306dc18930478c67b8ad631be
s-alad <n7.saad@gmail.com>
2022-11-28 19:43:59 -0500

    c

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -26,9 +26,10 @@ function Header() {
       <div className="flexspace"></div>
       <div className='underline'>
         <ul>
+        <li><a href="/">Home</a></li>
           <li><a href="/#about">About</a></li>
           <li><a href="/speaking">Speaking</a></li>
-          <li><a href="/appraisal">Appraisals</a></li>
+          {/* <li><a href="/appraisal">Appraisals</a></li> */}
           <li><a href="/media">Media</a></li>
           <li><a href="/#premiere">Premiere</a></li>
           <li><a href="/#contact">Contact</a></li>
@@ -36,7 +37,7 @@ function Header() {
       </div>
       <div className='divider'></div>
       <SocialIcon target={'_blank'} url='https://www.linkedin.com/in/jillian-white-sra-93a23b6/' bgColor='transparent' fgColor='white' className='ico'></SocialIcon>
-      <SocialIcon target={'_blank'} url='https://www.twitter.com/' bgColor='transparent' fgColor='white' className='ico'></SocialIcon>
+      {/* <SocialIcon target={'_blank'} url='https://www.twitter.com/' bgColor='transparent' fgColor='white' className='ico'></SocialIcon> */}
       {/* <div className="media item">leadership</div>
       <div className="media item">press</div>
       <div className="blog item">blog</div>
