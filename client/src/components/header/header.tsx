5c991b4f60fe7f1d2ea4d7e0a78a714ff94568bc
s-alad <n7.saad@gmail.com>
2022-12-02 02:32:22 -0500

    navbar mobile

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -20,10 +20,38 @@ function Header() {
     window.addEventListener('scroll', listenScrollEvent)
   }, []);
 
+  function popMenu() {
+    const menu = document.getElementsByClassName('mobile-navigation')[0];
+    menu.classList.toggle('pop');
+    const menuButton = document.getElementsByClassName('mobile-lines')[0];
+    menuButton.classList.toggle('gone');
+  }
 
+  function closeOut() {
+    const menu = document.getElementsByClassName('mobile-navigation')[0];
+    menu.classList.toggle('pop');
+    const menuButton = document.getElementsByClassName('mobile-lines')[0];
+    menuButton.classList.toggle('gone');
+  }
 
   return (
     <div className={'navigation' + filled}>
+      <div className='mobile-lines' onClick={() => popMenu()}>
+        <div className='line'></div>
+        <div className='line'></div>
+        <div className='line'></div>
+      </div>
+      <div className='mobile-navigation'>
+          <div className='exit' onClick={() => closeOut()}>X</div>
+            <a href="/" onClick={() => closeOut()}>Home</a>
+            <a href="/about" onClick={() => closeOut()}>About</a>
+            <a href="/speaking" onClick={() => closeOut()}>Speaking</a>
+            <a href="/#appraisal" onClick={() => closeOut()}>Appraisal</a>
+            <a href="/#premiere" onClick={() => closeOut()}>Premiere</a>
+            <a href="/media" onClick={() => closeOut()}>Media</a>
+            <a href="/#contact" onClick={() => closeOut()}>Contact</a>
+
+      </div>
       {/* <Logo></Logo> */}
       <div className="navflexspace"></div>
       <div className='underline'>
