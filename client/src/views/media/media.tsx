a2867c9d48bcc789030bfc3d2b0e5f7d8eb7b775
s-alad <n7.saad@gmail.com>
2022-12-02 17:11:11 -0500

    c

--- client/src/views/media/media.tsx
+++ client/src/views/media/media.tsx
@@ -50,6 +50,48 @@ function Work() {
         <div className='work' id='media'>
             <h1>In The News</h1>
             <Carousel responsive={responsive} className='works' itemClass="nowidth" >
+                
+                    <div className='peice'>
+                        <div className='pin'></div>
+                        <a href={"https://www.nbcnews.com/news/us-news/black-appraisers-call-out-industry-s-racial-bias-need-systemic-n1269452"} target="_blank">
+                            <div className='banner'>
+                                <img src={"http://images.ctfassets.net/wm1hz9m6fq76/2iF4a3ZrmgiLS4bdT04CVY/fddbca1f2cccd55fee59e929def3c62a/210607-jillian-white-mn-0905.jpg"} alt='work' />
+                            </div>
+
+
+                            <div className='data'>
+                                <div className='title'>
+                                    Black appraisers call out industry's racial bias and need for systemic change
+                                </div>
+                                <div className='flexspace'></div>
+                                <div className='description'>
+                                    NBC News
+                                </div>
+                                <div className='date'>2021-06-27</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className='peice'>
+                        <div className='pin'></div>
+                        <a href={"https://www.bloomberg.com/news/articles/2021-03-03/appraisers-acknowledge-bias-in-home-valuations"}>
+                            <div className='banner'>
+                                <img src={"http://images.ctfassets.net/wm1hz9m6fq76/5l3PWtkHhrH9zOBkGxre51/53123f23f27e1bd8c26139ee59c06b79/-1x-1.jpg"} alt='work' />
+                            </div>
+
+
+                            <div className='data'>
+                                <div className='title'>
+                                What It Will Take to Close the Race Gap in Home Appraisals
+                                </div>
+                                <div className='flexspace'></div>
+                                <div className='description'>
+                                    Bloomberg
+                                </div>
+                                <div className='date'>2021-03-03</div>
+                            </div>
+                        </a>
+                    </div>
+                
                 {
                     loading
                         ? <div>Loading...</div>
