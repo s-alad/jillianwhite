9a8a632ca65ed37f94733c9e219d5453bd645894
s-alad <n7.saad@gmail.com>
2022-12-07 13:58:27 -0500

    h-a award

---
+++ client/src/paths/Medium/Medium.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Medium.css';
+
+function Medium() {
+  return (
+    <div className='medium'>
+      {/* <h1>Medium</h1> */}
+      <div className='medium-listen'>
+        Take a listen to what Jillian has to talk about.
+      </div>
+    </div>
+  );
+}
+
+export default Medium;
