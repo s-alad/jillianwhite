eb20d52c0d6d69ba63273d5ab8e0299e84bc8c4b
s-alad <n7.saad@gmail.com>
2023-01-10 14:19:37 -0500

    analytics

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './views/App/App';
+import { inject } from '@vercel/analytics';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,7 +13,11 @@ root.render(
       false ?
       <div className='dev'>in development.</div>
       :
+      <>
+      {inject()}
       <App/>
+      </>
+      
     }
 
   </React.StrictMode>
